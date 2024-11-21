8d3a8a06552f02a8b886621bda9a59bb5f286187
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-21 11:22:46 -0800

    worked on 4.6

--- Main.java
+++ Main.java
@@ -20,8 +20,19 @@ public class Main
 	// This method should implement Algorithm 1 and print all neccessary data
 	public static void algorithm1(String input)
 	{
-		// TODO: Put solution to Algorithm 1 here
-		System.out.println("Implement me!");
+		input = input.toLowerCase();
+		String output = "";
+		String vowels = "aeiou";
+		int removedVowels = 0;
+		int repeatedVowels = 0;
+		int charactersSaved = 0;
+		
+		for (int i = 1; i < input.length(); i++) {
+			if (vowels.index)) {
+				if (input.substring())
+			}
+		}
+
 	}
 
 	// This method should implement Algorithm 2 and print all neccessary data
