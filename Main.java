455bc22ecda9e241c8f66fce63e1471927f60035
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-22 11:21:25 -0800

    worked some more on 4.6

--- Main.java
+++ Main.java
@@ -21,24 +21,50 @@ public class Main
 	public static void algorithm1(String input)
 	{
 		input = input.toLowerCase();
-		String output = "";
+		String output = input.substring(0, 1);
 		String vowels = "aeiou";
 		int removedVowels = 0;
-		int repeatedVowels = 0;
-		int charactersSaved = 0;
+		int repeatedConsonants = 0;
+		int charactersSaved = input.length();
 		
 		for (int i = 1; i < input.length(); i++) {
-			if (vowels.index)) {
-				if (input.substring())
+			if (vowels.indexOf(input.substring(i, i +1)) == -1 || input.substring(i - 1, i).equals(" ") ) {
+				if (!(input.substring(i, i + 1).equals(input.substring(i - 1, i)))) {
+					output += input.substring(i, i + 1);
+				}  else {
+					repeatedConsonants++;
+				}
+			} else {
+				removedVowels+=1;
 			}
-		}
+		} 
+
+		charactersSaved = input.length() - output.length();
+
+		System.out.println("Algorithm 1");
+		System.out.println("Vowels removed: " + removedVowels);
+		System.out.println("Repeats removed: " + repeatedConsonants);
+		System.out.println("Algorithm 1 message: " + output);
+		System.out.println("Algorithm 1 characters saved: " + charactersSaved);
 
 	}
 
 	// This method should implement Algorithm 2 and print all neccessary data
 	public static void algorithm2(String input)
 	{
-		// TODO: Put solution to Algorithm 1 here
-		System.out.println("Implement me!");
+		input = input.toLowerCase();
+		String output = "";
+		int count = 0;
+		int unique = 0;
+
+		for (int i = 0; i < input.length(); i++) {
+			String currentLetter = input.substring(i, i + 1);
+			
+			if (!(currentLetter.equals(" ") && output.indexOf(currentLetter) == -1)) {
+				
+
+				
+			}
+		}
 	}
 }
